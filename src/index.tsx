3845dfc6d9e689dfdb3a794f4c885fbc1f857da4
Newton Lomar <newtonlomar16@gmail.com>
2021-12-04 17:11:03 -0300

    configurando public

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import { Root } from './root'
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
-  document.getElementById('root'),
+  document.querySelector('[data-js="app"]'),
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
---
+++ src/root.tsx
@@ -0,0 +1,9 @@
+import { App } from './app'
+
+function Root () {
+  return (
+    <App />
+  )
+}
+
+export { Root }
