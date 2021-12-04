96de03c28cb57221eff9c1b01c503924fbd0a637
Newton Lomar <newtonlomar16@gmail.com>
2021-12-04 12:57:10 -0300

    configurando eslintdefault para errors do eslint travarem os commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg'
-import './App.css'
+import './App.css';
 
-function App (): string {
+function App () {
   return (
     <div className='App'>
       <header className='App-header'>
