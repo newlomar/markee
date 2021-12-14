c4b9b359e00e66ab7a2fde90f26a71ccb096f1bf
Newton Lomar <newtonlomar16@gmail.com>
2021-12-14 07:11:06 -0300

    markup and style of sidebar's header

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -3,20 +3,40 @@ import styled, { css } from 'styled-components/macro'
 function Sidebar () {
   return (
     <Aside>
-      <Title>Markee</Title>
+      <Header>
+        <Image src='logo192.png' alt='logo' />
+        <Title>markee<Span>.</Span></Title>
+      </Header>
     </Aside>
   )
 }
 
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5rem 3rem;
+`
+
 const Aside = styled.aside`${({ theme }) => css`
   height: 100vh;
+  width: 30rem;
   background: ${theme.colors.black}
 `}`
 
 const Title = styled.h1`${({ theme }) => css`
-  background: ${theme.colors.black};
-  color: ${theme.colors.primary};
+  font-size: 3rem;
+  color: ${theme.colors.white};
   margin: 0;
 `}`
+
+const Span = styled.span`${({ theme }) => css`
+  color: ${theme.colors.primary}
+`}`
+
+const Image = styled.img`
+  width: 4rem;
+  padding-right: 1rem;
+`
 
 export { Sidebar }
