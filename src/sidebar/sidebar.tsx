f19f17b8f05b4129d16654e660f2a7bcd60122e8
Newton Lomar <newtonlomar16@gmail.com>
2021-12-18 20:15:43 -0300

    Finished sidebar markup without action on button

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,6 +1,66 @@
 import styled, { css } from 'styled-components/macro'
 
 function Sidebar () {
+  interface Item {
+    id: string
+    name: string
+    content: string
+    active: boolean
+    status: 'editing' | 'saving' | 'saved'
+  }
+
+  const array: Item[] = [
+    {
+      id: '1',
+      name: 'Markdown 1',
+      content: 'sdadsadsa',
+      active: true,
+      status: 'editing',
+    },
+    {
+      id: '2',
+      name: 'Markdown 2',
+      content: 'sdadsadsa',
+      active: false,
+      status: 'saved',
+    },
+    {
+      id: '3',
+      name: 'Markdown 3',
+      content: 'sdadsadsa',
+      active: false,
+      status: 'saved',
+    },
+    {
+      id: '4',
+      name: 'Markdown 4',
+      content: 'sdadsadsa',
+      active: false,
+      status: 'saved',
+    },
+    {
+      id: '5',
+      name: 'Markdown 5',
+      content: 'sdadsadsa',
+      active: false,
+      status: 'saved',
+    },
+    {
+      id: '6',
+      name: 'Markdown 6',
+      content: 'sdadsadsa',
+      active: false,
+      status: 'saved',
+    },
+    {
+      id: '7',
+      name: 'Markdown 7',
+      content: 'sdadsadsa',
+      active: false,
+      status: 'saved',
+    },
+  ]
+
   return (
     <Aside>
       <Header>
@@ -17,63 +77,25 @@ function Sidebar () {
         </ArchiveSpan>
         <Button>+ <ButtonSpan>Adicionar arquivo</ButtonSpan></Button>
         <List>
-          <ListItem>
-            <Link href='/'>
-              <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-              </svg>
-              <LinkText>Exemplo 1</LinkText>
-            </Link>
-            <DeleteButton>x</DeleteButton>
-          </ListItem>
-          <ListItem>
-            <Link href='/'>
-              <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-              </svg>
-              <LinkText>Exemplo 1</LinkText>
-            </Link>
-            <DeleteButton>x</DeleteButton>
-          </ListItem>
-          <ListItem>
-            <Link href='/'>
-              <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z'
-                  stroke='white'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                />
-                <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-              </svg>
-              <LinkText>Exemplo 1</LinkText>
-            </Link>
-            <DeleteButton>x</DeleteButton>
-          </ListItem>
-          <ListItem>
-            <Link href='/'>
-              <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-              </svg>
-              <LinkText>Exemplo 1</LinkText>
-            </Link>
-            <DeleteButton>x</DeleteButton>
-          </ListItem>
+          {
+            array.map((item) => {
+              return (
+                <ListItem key={item.id}>
+                  <Link href='/'>
+                    <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                      <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                    </svg>
+                    <LinkText>{item.name}</LinkText>
+                  </Link>
+                  <DeleteButton>x</DeleteButton>
+                </ListItem>
+              )
+            })
+          }
         </List>
       </Section>
     </Aside>
