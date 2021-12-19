fbceeeea09eda205c014716c29ccd2a4dede26d5
Newton Lomar <newtonlomar16@gmail.com>
2021-12-19 10:39:50 -0300

    starting content markup

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -62,43 +62,46 @@ function Sidebar () {
   ]
 
   return (
-    <Aside>
-      <Header>
-        <Image src='logo192.png' alt='logo' />
-        <Title>markee<Span>.</Span></Title>
-      </Header>
-      <Section>
-        <ArchiveSpan>
-          <LineOne />
-          <Title2>
-            Arquivos
-          </Title2>
-          <LineTwo />
-        </ArchiveSpan>
-        <Button>+ <ButtonSpan>Adicionar arquivo</ButtonSpan></Button>
-        <List>
-          {
-            array.map((item) => {
-              return (
-                <ListItem key={item.id}>
-                  <Link href='/'>
-                    <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                      <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                      <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                      <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                      <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-                    </svg>
-                    <LinkText>{item.name}</LinkText>
-                  </Link>
-                  <DeleteButton>x</DeleteButton>
-                </ListItem>
-              )
-            })
-          }
-        </List>
-      </Section>
-    </Aside>
+    <>
+      <Aside>
+        <Header>
+          <Image src='logo192.png' alt='logo' />
+          <Title>markee<Span>.</Span></Title>
+        </Header>
+        <Section>
+          <ArchiveSpan>
+            <LineOne />
+            <Title2>
+              Arquivos
+            </Title2>
+            <LineTwo />
+          </ArchiveSpan>
+          <Button>+ <ButtonSpan>Adicionar arquivo</ButtonSpan></Button>
+          <List>
+            {
+              array.map((item) => {
+                return (
+                  <ListItem key={item.id}>
+                    <Link href='/'>
+                      <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                        <path d='M14 2.88062V8.88062H20' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                        <path d='M16 13.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                        <path d='M16 17.8806H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                        <path d='M10 9.88062H9H8' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
+                      </svg>
+                      <LinkText>{item.name}</LinkText>
+                    </Link>
+                    <DeleteButton>x</DeleteButton>
+                  </ListItem>
+                )
+              })
+            }
+          </List>
+        </Section>
+      </Aside>
+      <Content />
+    </>
   )
 }
 
@@ -227,5 +230,9 @@ const DeleteButton = styled.button`${({ theme }) => css`
     border-radius: 100px;
   };
 `}`
+
+const Content = styled.main`
+  background: red;
+`
 
 export { Sidebar }
