b2908e3eba084314543d33401b4504fe5edbc000
Newton Lomar <newtonlomar16@gmail.com>
2021-12-26 11:51:39 -0300

    file image on header update

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react'
+import { activeFile } from 'ui/icons'
 import styled, { css } from 'styled-components/macro'
 
 function Content () {
@@ -12,13 +13,7 @@ function Content () {
   return (
     <StyledSection>
       <StyledHeader>
-        <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-          <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' stroke='#1FC8E1' strokeLinecap='round' strokeLinejoin='round' />
-          <path d='M14 2.88062V8.88062H20' stroke='#1FC8E1' strokeLinecap='round' strokeLinejoin='round' />
-          <path d='M16 13.8806H8' stroke='#1FC8E1' strokeLinecap='round' strokeLinejoin='round' />
-          <path d='M16 17.8806H8' stroke='#1FC8E1' strokeLinecap='round' strokeLinejoin='round' />
-          <path d='M10 9.88062H9H8' stroke='#1FC8E1' strokeLinecap='round' strokeLinejoin='round' />
-        </svg>
+        <LinkImage />
         <Input type='text' />
       </StyledHeader>
       <Main>
@@ -42,6 +37,10 @@ const StyledHeader = styled.header`
   display: flex;
   align-items: center;
   width: 100%;
+`
+
+const LinkImage = styled.img`
+  content:url("${activeFile}")
 `
 
 const Input = styled.input`
